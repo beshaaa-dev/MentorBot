b2b2423ea6e0fb95cd77b0a76fbb35ffc3d52423
Andrey Buchevskiy <w2wmatch@gmail.com>
2025-12-03 14:27:33 +0300

    Исправлена обработка диалога со студентом

--- handlers/student.py
+++ handlers/student.py
@@ -191,5 +191,5 @@ def create_student_conversation_handler(
             ],
         },
         fallbacks=[CommandHandler("cancel", cancel_video)],
-        conversation_timeout=60 * 60,  # 1 hour
+        allow_reentry=True,  # Allow /start to restart conversation
     )
